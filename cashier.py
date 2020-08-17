3dde248af2984339c0ddf250569168c3d4bc772b
atheen aleesa <atheen@atheens-iMac.local>
2020-08-17 09:43:20 +0300

    done cashier

--- cashier.py
+++ cashier.py
@@ -1,5 +1,24 @@
 def main():
 	# write code here
+	items_list = []
+	item = input("Item (enter \"done\" when finished): ")
+	while item != "done":
+		price = input("Price: ")
+		quan = input("Quantity: ")
+		dict = {"name": item, "price": price, "quantity": quan}
+		items_list.append(dict)
+		item = input("Item (enter \"done\" when finished): ")
+	total_price = 0
+	print("\n-------------------\nreceipt\n-------------------")
+	for i in items_list:
+		price = float(i["quantity"])*float(i["price"])
+		total_price += price
+		print("%s %s %.3fKD"%(i["quantity"],i["name"],price))
+	print("-------------------\nTotal Price: %.3fKD"%(total_price))
+
+
+
+
 
 if __name__ == '__main__':
 	main()
